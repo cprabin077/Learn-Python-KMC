8d899cfe7bf045a1392da814bd2dcf58e38f36cd
= <prabin.chaudhary3107@gmail.com>
2026-04-15 15:01:59 +0545

    branch testing modified again and again

--- day4.py
+++ day4.py
@@ -1,9 +1,19 @@
 # Check the number is even or odd
 
-num = int(input("Enter the number: "))
+# num = int(input("Enter the number: "))
 
-if(num % 2 == 0):
-    print(num,"is even number")
-else:
-    print(num,"is odd number")
+# if(num % 2 == 0):
+#     print(num,"is even number")
+# else:
+#     print(num,"is odd number")
  
+# Check wthether the number is positive or not
+
+# num1 = float(input("Enter the number: "))
+
+# if(num1 == 0):
+#     print(f"The number {num1} is neither positive nor negative")
+# elif(num1 > 0):
+#     print(f"The number {num1} is positive")
+# else:
+#     print(f"The number {num1} is negative")
