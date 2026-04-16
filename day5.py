25e6bfb7f357523d0160057e5db9bc95f1873c4c
= <prabin.chaudhary3107@gmail.com>
2026-04-16 14:38:33 +0545

    list value  type checking

--- day5.py
+++ day5.py
@@ -1,13 +1,20 @@
-list1 = [1,2,3,4,5,6,7,8]
+# list1 = [1,2,3,4,5,6,7,8]
 
-print(type(list1))
+# print(type(list1))
 
-print(list1) # [1,2,3,4,5,6,7,8]
-print(list1[0]) # [1]
-print(list1[-8]) # [1]
+# print(list1) # [1,2,3,4,5,6,7,8]
+# print(list1[0]) # [1]
+# print(list1[-8]) # [1]
 
-print(list1[7]) # [8]
-print(list1[-7]) # [2]
+# print(list1[7]) # [8]
+# print(list1[-7]) # [2]
 
-print(list1[9]) # IndexError: list index out of range
+# # print(list1[9]) # IndexError: list index out of range
+
+# print(len(list1))
+
+lst2 = [1,3,"Ram", 4.6, "Orange", True, False, 3.14]
+print(isinstance(lst2[-1], float))
+
+print(isinstance(lst2[len(lst2)-1], float))
 
