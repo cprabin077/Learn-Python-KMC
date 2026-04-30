73545d6a8854e145f7d46ec7393e872fb037695b
= <prabin.chaudhary3107@gmail.com>
2026-04-30 14:56:44 +0545

    List comprehension

--- day16.py
+++ day16.py
@@ -37,3 +37,22 @@ square = lambda *args: [i**2 for i in args]
 print(square(1,2,3,4,5,6,7,8,9)) # [1, 4, 9, 16, 25, 36, 49, 64, 81]
 print(square(15,16)) # [225, 256]
 
+# List Comprehension probem
+list1 = [1,2,3,4,5]
+a = [i for i in list1]
+print(f"The list of numbers are:\n{a}")
+a = [i**2 for i in list1]
+print(f"The list of square numbers are:\n{a}")
+a = [i**3 for i in list1]
+print(f"The list of cubic numbers are:\n{a}")
+
+list2 = [i for i in range(1,20) if i % 2 == 0]
+print(f"The list of even numbers are:\n{list2}")
+list2 = [i for i in range(1,20) if i % 2 != 0]
+print(f"The list of odd numbers are:\n{list2}")
+
+
+list3 = [1,2,3,4,5]
+data = [i+10 for i in list3]
+print(f"The required output is:\n{data}")
+
