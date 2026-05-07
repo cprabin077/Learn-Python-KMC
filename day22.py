abf0a6d69568715954cbc3ca557f277b13a51d22
= <prabin.chaudhary3107@gmail.com>
2026-05-07 22:16:05 +0545

    SQlite Database connectivity

--- day22.py
+++ day22.py
@@ -11,14 +11,47 @@
 # CRUD
 import sqlite3
 
-
+# Connect database
 connection = sqlite3.connect('students.sqlite3')
+
+# Create cursor
 terminal = connection.cursor()
 
-#INSERT Query
-query = "INSERT INTO students (name, age, course, marks, email) VALUES ('Rita Chaudhary', 23, 'BCA', 65, 'ritachy@gmail.com')"
+# Create table
+terminal.execute("""
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    course TEXT NOT NULL,
+    marks INTEGER NOT NULL,
+    email TEXT NOT NULL
+)
+""")
 
-terminal.execute(query)
+# Insert query
+query = """
+INSERT INTO students (name, age, course, marks, email)
+VALUES (?, ?, ?, ?, ?)
+"""
+
+data = ('Rita Chaudhary', 23, 'BCA', 65, 'ritachy@gmail.com')
+
+terminal.execute(query, data)
+
+# Save changes
 connection.commit()
-print(connection)
+
+print("Data inserted successfully")
+
+# Display data
+terminal.execute("SELECT * FROM students")
+
+rows = terminal.fetchall()
+
+for row in rows:
+    print(row)
+
+# Close connection
+connection.close()
 
