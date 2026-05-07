69436e1f8def9c44a355816aa002a2090b10ae0c
= <prabin.chaudhary3107@gmail.com>
2026-05-07 15:03:36 +0545

    Student Management System Access modifier with inheritance

---
+++ assignment_day21.py
@@ -0,0 +1,30 @@
+class BankAccount():
+    account_holder = "XYZ"
+    __balance = 1000
+
+    def deposit(self, amount):
+        self.__balance += amount
+        return f"Name of Account holder is {self.account_holder}\nThe deposited amount is {amount}\nTotal balance is {self.__balance}"
+    
+    def withdraw(self, amount):
+        self.__balance -= amount
+        return f"The fined amount is {amount}"
+    
+    def showBalance(self):
+        return f"The remaining balance is {self.__balance}"
+    
+class StudentAccount(BankAccount):
+    studentId = 101
+
+    def pay_library_fine(self, amount):
+        return self.withdraw(amount)
+    
+obj = StudentAccount()
+print(obj.deposit(200))
+print(obj.pay_library_fine(100))
+print(obj.showBalance())
+
+        
+
+
+
