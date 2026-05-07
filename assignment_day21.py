57602dd453b7ff3513944fe47844b2e10f733b38
= <prabin.chaudhary3107@gmail.com>
2026-05-07 15:11:19 +0545

    Inheritance with access modifier

--- assignment_day21.py
+++ assignment_day21.py
@@ -24,6 +24,15 @@ print(obj.deposit(200))
 print(obj.pay_library_fine(100))
 print(obj.showBalance())
 
+''' # Output
+Name of Account holder is XYZ
+The deposited amount is 200
+Total balance is 1200
+The fined amount is 100
+The remaining balance is 1100
+
+'''
+
         
 
 
