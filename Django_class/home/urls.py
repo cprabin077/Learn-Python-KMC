e6bbd1e9bd0daba7ca613bd15e235d9196b19bda
= <prabin.chaudhary3107@gmail.com>
2026-05-15 15:49:08 +0545

    simple day1 django with basic dyanic as well as static rendering

---
+++ Django_class/home/urls.py
@@ -0,0 +1,9 @@
+from home.views import home, home_json, home_page
+from django.urls import path
+
+
+urlpatterns = [
+    path('home/', home, name='home'),
+    path('json/', home_json, name='json-data'),
+    path('home_page/',home_page, name = 'page')
+]
--- Django_class/home/views.py
+++ Django_class/home/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def home(request):
+    print("Hello from  Django!!")
+    return HttpResponse("<h1> Hello from  Django!! </h1>")
+
+
+def home_json(request):
+    data = {
+        "name": "Prabin Chaudhary",
+        "address": "Kathmandu"
+    }
+    return JsonResponse(data)
+
+
+def home_page(request):
+    user_info = {
+        'name':'Prabin Chaudhary'
+    }
+    return render(request, "home/index.html",user_info)
+
+
