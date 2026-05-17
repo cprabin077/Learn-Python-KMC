b584f4d9f4aa54f386b16644b6db9b79e23cfd6e
= <prabin.chaudhary3107@gmail.com>
2026-05-17 15:49:55 +0545

    create db table, insert data, CRUD using ORM

--- Django_class/home/admin.py
+++ Django_class/home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from home.models import Student
+
 # Register your models here.
+# admin.site.register(Student)
+
+@admin.register(Student)
+class Student_admin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'dob', 'number', 'is_active']
+    search_fields = ['name', 'dob']
+    list_filter = ['is_active','name']
--- Django_class/home/models.py
+++ Django_class/home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100, 
+                            null=True, blank=True, 
+                            help_text="student full name", 
+                            verbose_name="Student Name")
+    dob = models.DateField()
+    number = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+    class Meta:
+        db_table = "student" # mathiko table name lai 'student' table ma override garnalai
